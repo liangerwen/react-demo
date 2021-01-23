3f029aed2c9d10e994db40dc553cf2074ee21ecf
michaelzhou <michaelzhou@aie-tec.com>
2021-01-23 17:28:42 +0800

    111

--- src/components/B.jsx
+++ src/components/B.jsx
@@ -1,7 +1,64 @@
-export default function B() {
+import React from "react";
+import { Menu } from "antd";
+import "./b.less";
+const { SubMenu } = Menu;
+export default function B(props) {
+  console.log(props)
+  let mr = [
+    {
+      name: "1",
+      path: "1",
+      ch: [
+        {
+          name: "1.1",
+          path: "1.1",
+          ch: [
+            {
+              name: "1.1.1",
+              path: "1.1.1",
+            },
+            {
+              name: "1.1.2",
+              path: "1.1.2",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "2",
+      path: "2",
+    },
+  ];
+  const diffMeno = (r) => {
     return (
-      <div>
-        BBBBBBBBBBBB
-      </div>
+      <>
+        {r.map((rr) => {
+          if (rr.ch)
+            return (
+              <SubMenu key={rr.name} title={rr.path}>
+                {diffMeno(rr.ch)}
+              </SubMenu>
+            );
+          else
+            return (
+              <Menu.Item key={rr.name}>{rr.path}</Menu.Item>
+            );
+        })}
+      </>
     );
-  }
+  };
+
+  return (
+    <>
+      <h1>BBBBBBBBBB</h1>
+      <h1>BBBBBBBBBB</h1>
+      <h1>BBBBBBBBBB</h1>
+      <h1>BBBBBBBBBB</h1>
+      <h1>BBBBBBBBBB</h1>
+      <h1>BBBBBBBBBB</h1>
+      <h1>BBBBBBBBBB</h1>
+      {/* {diffMeno(mr)} */}
+    </>
+  );
+}
